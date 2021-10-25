34e0e4f6cdee0114eb954c50bb372e5d40bb887c
Nafis Hasan Tonmoy <57575805+Nafis71@users.noreply.github.com>
2021-10-25 13:41:26 +0600

    Update customer.php
    Style changed

--- customer.php
+++ customer.php
@@ -15,7 +15,7 @@ session_start();
     <title>Tech Gear</title>
 </head>
 <body>
-
+<header id="header">
     <div class="menu-bar">
         <ul>
             <li><a href="index.php"><i class="fa fa-home "></i> Home</a></li>
@@ -26,16 +26,33 @@ session_start();
             <form action="search.php" method="POST">
             <input type="text" name="search" class="form-control" placeholder="Search The Store" autocomplete="off" required>
             <button type="submit" name="submit" class="btn btn-primary">Search</button>
+            </ul>
 </div>
-</form>
-	</div>
-             
-        </ul>
-</div>
+
+            <nav class="navbar">
+            <ul>
+            <li><a href="index.php">Processor</a></li>
+            
+            <li><a href="login.php">Graphics&nbsp;Card</a></li>
+            <li><a href="#">Mouse</a></li>
+            <li><a href="#">Monitor</a></li>
+            <li><a href="#">Headphone</a></li>
+            <li><a href="#">CC&nbsp;Camera</a></li>
+            <li><a href="#">Ear&nbsp;Phone</a></li>
+            <li><a href="#">Web&nbsp;Cam</a></li>
+            <li><a href="#">Capture&nbsp;Card</a></li>
+            <li><a href="#">Power&nbsp;Bank</a></li>
+            <li><a href="#">TV&nbsp;Card</a></li>
+            <li><a href="#">DSLR&nbsp;Camera</a></li>
+            <li><a href="#">Action&nbsp;Camera</a></li>
+
+            </ul>
+</nav>
+</header>
 <div class="container">
     <div clas="row">
 <?php
-include 'connect.php';
+include'connect.php';
 mysqli_select_db($connection,'store');
 $query = "select *from product";
 $result = mysqli_query($connection,$query);
