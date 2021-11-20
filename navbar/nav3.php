26148664ce4bbc37909ebf32bf05fd3e40b6b33f
Nafis71 <nhtonmoy2@gmail.com>
2021-11-21 05:27:00 +0600

    Navbar updated

--- navbar/nav3.php
+++ navbar/nav3.php
@@ -60,8 +60,8 @@ while($fetch = mysqli_fetch_assoc($result))
    
    }
    else{
-   echo'<button type="button" class="btn btn-primary "><i class="fas fa-shopping-cart"></i>Buy Now</button>';
-   echo'<button type="button" class="btn btn-warning "><i class="fas fa-shopping-bag"></i>Add to Cart</button>';
+    echo'<button type="button" class="btn btn-primary "><a href="payment.php?productid='.$id.'"class="text-light"><i class="fas fa-shopping-cart"></i>Buy Now</a></button>';
+    echo'<button type="button" class="btn btn-warning "><a href="cartpayment.php?productid='.$id.'"class="text-decoration-none"><i class="fas fa-shopping-bag"></i>Add to Cart</a></button>';
    }?>
    </div>
 </div>
